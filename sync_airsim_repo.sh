6ff3d6735eab7f06f958fc10886eaeb7cda2968b
Seth Nielsen <sethmnielsen@gmail.com>
2021-04-30 12:26:06 -0600

    Fixed sync script

--- sync_airsim_repo.sh
+++ sync_airsim_repo.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [[ -z "${AIRSIMPATH}" ]]; then
     echo ""
     echo "AIRSIMPATH not set."
@@ -7,20 +9,30 @@ if [[ -z "${AIRSIMPATH}" ]]; then
 fi
 
 set -e
+set -x
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd "$SCRIPT_DIR" >/dev/null
+
+# Sync changes to AirLib source files
 rsync -a --delete Source/AirLib/include $AIRSIMPATH/AirLib
 rsync -a --delete Source/AirLib/src     $AIRSIMPATH/AirLib
 
-rsync -a --delete Source/*.h    $AIRSIMPATH/Unreal/Plugins/AirSim/Source
-rsync -a --delete Source/*.cpp  $AIRSIMPATH/Unreal/Plugins/AirSim/Source
 
-rsync -a --delete Source/Recording \\
-                  Source/SimHUD \\
-                  Source/SimJoyStick \\
-                  Source/SimMode  \\
-                  Source/UnrealSensors \\
-                  Source/Vehicles \\
-                  Source/Weather \\
-                  $AIRSIMPATH/Unreal/Plugins/AirSim/Source
+# Sync changes to AirSim plugin
+# These are listed explicitly to avoid copying Source/AirLib to $plugin_source
+plugin_source=$AIRSIMPATH/Unreal/Plugins/AirSim/Source
+
+rsync -a --delete Source/*.h Source/*.cpp $plugin_source
+rsync -a --delete Source/Recording        $plugin_source
+rsync -a --delete Source/SimHUD           $plugin_source
+rsync -a --delete Source/SimJoyStick      $plugin_source
+rsync -a --delete Source/SimMode          $plugin_source
+rsync -a --delete Source/UnrealSensors    $plugin_source
+rsync -a --delete Source/Vehicles         $plugin_source
+rsync -a --delete Source/Weather          $plugin_source
+
+# Sync changes to Tiltrotor assets
+rsync -a --delete Content/Tiltrotor $AIRSIMPATH/Unreal/Plugins/AirSim/Content
+
+popd >/dev/null
