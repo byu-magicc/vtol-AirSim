2381e8f9bb0faca8dfe4c5ec3241b76eee35ec83
Seth Nielsen <sethmnielsen@gmail.com>
2021-07-21 11:38:46 -0600

    Merged clang formatted code

--- Source/AirsimLevelStreaming.h
+++ Source/AirsimLevelStreaming.h
@@ -2,12 +2,11 @@
 #include "Runtime/Engine/Classes/Engine/LevelStreamingDynamic.h"
 #include "Misc/PackageName.h"
 
-
 class UAirsimLevelStreaming : public ULevelStreamingDynamic
 {
 public:
-	static UAirsimLevelStreaming* LoadAirsimLevelInstance(UWorld* WorldContextObject, FString LevelName, FVector Location, FRotator Rotation, bool& bOutSuccess);
+    static UAirsimLevelStreaming* LoadAirsimLevelInstance(UWorld* WorldContextObject, FString LevelName, FVector Location, FRotator Rotation, bool& bOutSuccess);
 
 private:
-	static int32 LevelInstanceId;
+    static int32 LevelInstanceId;
 };
